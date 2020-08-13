b314f2ef2c2469a98fd67a2df8309e3c22b6acf4
Ewan Danks <ewan.danks@fdmgroup.com>
2020-08-13 18:10:32 +0100

    added toggle betwween oscs

--- WavetableOscillator.h
+++ WavetableOscillator.h
@@ -61,21 +61,30 @@ public:
         addAndMakeVisible(frequencySlider);
         frequencySlider.setRange(50.0, 20000.0);
         frequencySlider.setSkewFactorFromMidPoint(500.0); 
-        frequencySlider.setBounds(100, 20, 200, 200);
+        frequencySlider.setBounds(20, 50, 200, 200);
         frequencySlider.onValueChange = [this]
         {
             oscFreq = frequencySlider.getValue();
         };
+
+        //toggle 
+        addAndMakeVisible(sineToggle);
+        sineToggle.setRange(0.0, 1.0);
+        sineToggle.onValueChange = [this]
+        {
+            sineToggle.getValue() < 0.5 ? sine = false : sine = true;
+        };
+        sineToggle.setBounds(20, 200, 200, 200);
 
         cpuUsageLabel.setText("CPU Usage", juce::dontSendNotification);
         cpuUsageText.setJustificationType(juce::Justification::right);
         addAndMakeVisible(cpuUsageLabel);
         addAndMakeVisible(cpuUsageText);
 
-        //createHarmonicSineWavetable();
+        createHarmonicSineWavetable();
         createSquareWaveTable();
 
-        setSize(400, 200);
+        setSize(300, 400);
         setAudioChannels(0, 2); 
         startTimer(50);
     }
@@ -143,12 +152,24 @@ public:
 
     void prepareToPlay(int, double sampleRate) override
     {
-        auto* oscillator = new WavetableOscillator(squareTable);
-        oscillators.add(oscillator);
+        auto* oscillator1 = new WavetableOscillator(squareTable);
+        auto* oscillator2 = new WavetableOscillator(sineTable);
+        oscillators.add(oscillator1);
+        oscillators.add(oscillator2);
         level = 0.25f;
     }
 
     void releaseResources() override {}
+
+    WavetableOscillator* getOscillator()
+    {
+        if (sine)
+            return oscillators.getUnchecked(0);
+        else
+            return oscillators.getUnchecked(1);
+    }
+
+
 
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override
     {
@@ -157,7 +178,7 @@ public:
 
         bufferToFill.clearActiveBufferRegion();
 
-        auto* oscillator = oscillators.getUnchecked(0);
+        WavetableOscillator* oscillator = getOscillator();
         oscillator->setFrequency(oscFreq, (float)44100);
 
         for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
@@ -174,12 +195,15 @@ private:
     juce::Label cpuUsageText;
 
     const unsigned int tableSize = 1 << 7;
-    float level = 0.0f, oscFreq = 440.0;;
+    float level = 0.0f, oscFreq = 50.0;
+    bool sine = false;
 
     juce::AudioSampleBuffer sineTable;
     juce::AudioSampleBuffer squareTable;
     juce::OwnedArray<WavetableOscillator> oscillators;
+
     juce::Slider frequencySlider;
+    juce::Slider sineToggle;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainContentComponent)
 };
